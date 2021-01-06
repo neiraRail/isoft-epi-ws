99d0da9160347bbe6087bf3fdd643a979401c9ad
raguileoam <a.melillan02@ufromail.cl>
2021-01-06 11:40:15 -0300

    Proyecto inicial

---
+++ README.md
@@ -0,0 +1,5 @@
+# epi-ws
+## Instrucciones
+- Instalar base de datos postgres o usar similar en docker.
+- Ingresar/modificar contraseña "123asd" al usuario "postgres". 
+- Crear una base de datos en bajo el nombre "bd_epi"
---
+++ src/main/java/cl/ufro/dci/epiws/EpiWsApplication.java
@@ -0,0 +1,14 @@
+package cl.ufro.dci.epiws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class EpiWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpiWsApplication.class, args);
+	}
+
+}
