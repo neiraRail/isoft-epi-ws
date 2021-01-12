6e5832895dd9934a4e3f932c24d577083aff48eb
neiraRail <53882048+neiraRail@users.noreply.github.com>
2021-01-12 14:33:13 -0300

    Se añade clase repositorio y clase servicio

--- src/main/java/cl/ufro/dci/epiws/repository/AntecedenteRepository.java
+++ src/main/java/cl/ufro/dci/epiws/repository/AntecedenteRepository.java
@@ -1,4 +1,9 @@
 package cl.ufro.dci.epiws.repository;
 
-public interface AntecedenteRepository {
+import cl.ufro.dci.epiws.model.Antecedente;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AntecedenteRepository extends CrudRepository<Antecedente, Long> {
 }
