20369a3ee5c412d0c474f4165c6605ad54ad0192
neiraRail <53882048+neiraRail@users.noreply.github.com>
2021-01-15 17:19:12 -0300

    Se arregla nombres de paquete e imports en las clases

---
+++ src/main/java/cl/ufro/dci/epiws/model/Region.java
@@ -0,0 +1,56 @@
+package cl.ufro.dci.epiws.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "region")
+public class Region {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "rgn_id")
+    private Long rgnId;
+
+    @Column(name = "rgn_nombre")
+    private String rgnNombre;
+
+    @OneToMany(mappedBy = "region")
+    private List<Comuna> comunaList;
+
+    public Region() {
+    }
+
+    public Region(String rgnNombre) {
+        this.rgnNombre = rgnNombre;
+    }
+
+    public Region(Long rgnId, String rgnNombre) {
+        this.rgnId = rgnId;
+        this.rgnNombre = rgnNombre;
+    }
+
+    public Long getRgnId() {
+        return rgnId;
+    }
+
+    public void setRgnId(Long rgnId) {
+        this.rgnId = rgnId;
+    }
+
+    public String getRgnNombre() {
+        return rgnNombre;
+    }
+
+    public void setRgnNombre(String rgnNombre) {
+        this.rgnNombre = rgnNombre;
+    }
+
+    public List<Comuna> getComunaList() {
+        return comunaList;
+    }
+
+    public void setComunaList(List<Comuna> comunaList) {
+        this.comunaList = comunaList;
+    }
+}
