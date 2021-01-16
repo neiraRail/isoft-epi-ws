8a53d60ef773d25dfc02e4acf8ed4110301be852
pablo <p.luer01@ufromail.cl>
2021-01-15 22:51:04 -0300

    Agregado modelo de evaluación 2.

---
+++ src/main/java/cl/ufro/dci/epiws/model/Antecedente.java
@@ -0,0 +1,117 @@
+package cl.ufro.dci.epiws.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "antecedente")
+public class Antecedente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ant_id")
+    private Long antId;
+
+    @ManyToOne
+    @JoinColumn(name = "pac_rut")
+    private Paciente paciente;
+
+    @Column(name = "ant_embarazo")
+    private Boolean antEmbarazo;
+
+
+    @Column(name = "and_enfermedad_cronica")
+    private String antEnfermedadCronica;
+
+    @Column(name = "ant_alergias")
+    private String antAlergias;
+
+    @Column(name = "ant_tipo_sangre")
+    private String antTipoSangre;
+
+    @Column(name = "ant_medicamentos")
+    private String antMedicamentos;
+
+    @Column(name = "ant_viaje_al_extranjero")
+    private String antViajeExtranjero;
+
+    
+    public Antecedente(Long antId, Paciente paciente, Boolean antEmbarazo, String antEnfermedadCronica, String antAlergias, String antTipoSangre, String antMedicamentos, String antViajeExtranjero) {
+        this.antId = antId;
+        this.paciente = paciente;
+        this.antEmbarazo = antEmbarazo;
+        this.antEnfermedadCronica = antEnfermedadCronica;
+        this.antAlergias = antAlergias;
+        this.antTipoSangre = antTipoSangre;
+        this.antMedicamentos = antMedicamentos;
+        this.antViajeExtranjero = antViajeExtranjero;
+    }
+
+    public Antecedente(){
+
+    }
+
+    public Long getAntId() {
+        return antId;
+    }
+
+    public void setAntId(Long antId) {
+        this.antId = antId;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public Boolean getAntEmbarazo() {
+        return antEmbarazo;
+    }
+
+    public void setAntEmbarazo(Boolean antEmbarazo) {
+        this.antEmbarazo = antEmbarazo;
+    }
+
+    public String getAntEnfermedadCronica() {
+        return antEnfermedadCronica;
+    }
+
+    public void setAntEnfermedadCronica(String antEnfermedadCronica) {
+        this.antEnfermedadCronica = antEnfermedadCronica;
+    }
+
+    public String getAntAlergias() {
+        return antAlergias;
+    }
+
+    public void setAntAlergias(String antAlergias) {
+        this.antAlergias = antAlergias;
+    }
+
+    public String getAntTipoSangre() {
+        return antTipoSangre;
+    }
+
+    public void setAntTipoSangre(String antTipoSangre) {
+        this.antTipoSangre = antTipoSangre;
+    }
+
+    public String getAntMedicamentos() {
+        return antMedicamentos;
+    }
+
+    public void setAntMedicamentos(String antMedicamentos) {
+        this.antMedicamentos = antMedicamentos;
+    }
+
+    public String getAntViajeExtranjero() {
+        return antViajeExtranjero;
+    }
+
+    public void setAntViajeExtranjero(String antViajeExtranjero) {
+        this.antViajeExtranjero = antViajeExtranjero;
+    }
+}
--- src/main/java/cl/ufro/dci/epiws/model/Establecimiento.java
+++ src/main/java/cl/ufro/dci/epiws/model/Establecimiento.java
@@ -1,4 +1,87 @@
 package cl.ufro.dci.epiws.model;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "establecimiento")
 public class Establecimiento {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "est_id")
+    private Long estId;
+
+    @Column(name = "est_nombre")
+    private String estNombre;
+
+    @Column(name = "est_direccion")
+    private String estDireccion;
+
+    @ManyToOne
+    @JoinColumn(name = "com_id")
+    private Comuna comuna;
+
+    @OneToMany(mappedBy = "establecimiento")
+    private List<Paciente> pacienteList;
+
+
+
+    public Establecimiento(Long estId, String estNombre, String estDireccion, Comuna comuna) {
+        this.estId = estId;
+        this.estNombre = estNombre;
+        this.estDireccion = estDireccion;
+        this.comuna = comuna;
+    }
+
+    public Establecimiento() {
+
+    }
+    //constructor sin id.
+    public Establecimiento(String estNombre, String estDireccion, Comuna comuna, List<Paciente> pacienteList) {
+        this.estNombre = estNombre;
+        this.estDireccion = estDireccion;
+        this.comuna = comuna;
+        this.pacienteList = pacienteList;
+    }
+
+    public Long getEstId() {
+        return estId;
+    }
+
+    public void setEstId(Long estId) {
+        this.estId = estId;
+    }
+
+    public String getEstNombre() {
+        return estNombre;
+    }
+
+    public void setEstNombre(String estNombre) {
+        this.estNombre = estNombre;
+    }
+
+    public String getEstDireccion() {
+        return estDireccion;
+    }
+
+    public void setEstDireccion(String estDireccion) {
+        this.estDireccion = estDireccion;
+    }
+
+    public Comuna getComuna() {
+        return comuna;
+    }
+
+    public void setComuna(Comuna comuna) {
+        this.comuna = comuna;
+    }
+
+    public List<Paciente> getPacienteList() {
+        return pacienteList;
+    }
+
+    public void setPacienteList(List<Paciente> pacienteList) {
+        this.pacienteList = pacienteList;
+    }
 }
