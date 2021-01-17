27111cdb80c30385f630f010bf1928e75f0f2a75
pablo <p.luer01@ufromail.cl>
2021-01-17 17:47:34 -0300

    Se eliminaron clases que no pertenecen a la funcionalidad.

--- src/main/java/cl/ufro/dci/epiws/repository/AntecedenteRepository.java
+++
@@ -1,9 +0,0 @@
-package cl.ufro.dci.epiws.repository;
-
-import cl.ufro.dci.epiws.model.Antecedente;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AntecedenteRepository extends CrudRepository<Antecedente, Long> {
-}
--- src/main/java/cl/ufro/dci/epiws/repository/CasoRepository.java
+++
@@ -1,9 +0,0 @@
-package cl.ufro.dci.epiws.repository;
-
-import cl.ufro.dci.epiws.model.Caso;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CasoRepository extends CrudRepository<Caso, Long> {
-}
